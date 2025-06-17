17409703d8ae6f823d66e588b4f8c1fe1ca37e14
pavelolofinskiy <pavelolofinskiy@gmail.com>
2025-06-17 20:46:28 +0300

    tests feature

--- tests/ApiControllerTest.php
+++ tests/ApiControllerTest.php
@@ -1,24 +1,34 @@
 <?php
 
+declare(strict_types=1);
+
 use PHPUnit\Framework\TestCase;
 use Src\Controllers\ApiController;
 use Src\Cache\RedisCache;
 
 class ApiControllerTest extends TestCase
 {
-    public function testGetFiltersWithCountsReturnsCachedData()
+    public function testGetFiltersWithCountsReturnsCachedData(): void
     {
         $pdo = $this->createMock(PDO::class);
         $redisCache = $this->createMock(RedisCache::class);
 
-        $cachedFilters = ['color' => ['name' => 'Color', 'values' => [], 'slug' => 'color']];
+        $cachedFilters = [
+            'color' => [
+                'name' => 'Color',
+                'values' => [],
+                'slug' => 'color',
+            ],
+        ];
+
         $redisCache->expects($this->once())
             ->method('get')
             ->with('filters_with_counts')
             ->willReturn($cachedFilters);
 
-        // PDO->query не должен вызываться, т.к. есть кеш
-        $pdo->expects($this->never())->method('query');
+        // Метод query у PDO не должен вызываться, т.к. данные берутся из кеша
+        $pdo->expects($this->never())
+            ->method('query');
 
         $controller = new ApiController();
         $result = $controller->getFiltersWithCounts($pdo, $redisCache);
@@ -26,7 +36,7 @@ class ApiControllerTest extends TestCase
         $this->assertSame($cachedFilters, $result);
     }
 
-    public function testGetFiltersWithCountsFetchesFromDbAndCaches()
+    public function testGetFiltersWithCountsFetchesFromDbAndCaches(): void
     {
         $pdo = $this->createMock(PDO::class);
         $redisCache = $this->createMock(RedisCache::class);
@@ -36,21 +46,24 @@ class ApiControllerTest extends TestCase
             ->with('filters_with_counts')
             ->willReturn(null);
 
-        // Мокаем query (parameters)
         $stmtParams = $this->createMock(PDOStatement::class);
         $pdo->expects($this->once())
             ->method('query')
-            ->with("SELECT id, name, slug FROM parameters")
+            ->with('SELECT id, name, slug FROM parameters')
             ->willReturn($stmtParams);
 
         $parameters = [
-            ['id' => 1, 'name' => 'Color', 'slug' => 'color'],
+            [
+                'id' => 1,
+                'name' => 'Color',
+                'slug' => 'color',
+            ],
         ];
+
         $stmtParams->expects($this->once())
             ->method('fetchAll')
             ->willReturn($parameters);
 
-        // Мокаем prepare и execute для parameter_values
         $stmtValues = $this->createMock(PDOStatement::class);
         $pdo->expects($this->once())
             ->method('prepare')
@@ -62,21 +75,31 @@ class ApiControllerTest extends TestCase
             ->with([1]);
 
         $values = [
-            ['value' => 'Red', 'product_count' => 10],
-            ['value' => 'Blue', 'product_count' => 5],
+            [
+                'value' => 'Red',
+                'product_count' => 10,
+            ],
+            [
+                'value' => 'Blue',
+                'product_count' => 5,
+            ],
         ];
+
         $stmtValues->expects($this->once())
             ->method('fetchAll')
             ->willReturn($values);
 
         $redisCache->expects($this->once())
             ->method('set')
-            ->with('filters_with_counts', $this->callback(function($filters) {
-                // Проверим, что в итоговом массиве есть нужные ключи
-                return isset($filters['color']) &&
-                       $filters['color']['name'] === 'Color' &&
-                       count($filters['color']['values']) === 2;
-            }), 600);
+            ->with(
+                'filters_with_counts',
+                $this->callback(function (array $filters): bool {
+                    return isset($filters['color'])
+                        && $filters['color']['name'] === 'Color'
+                        && count($filters['color']['values']) === 2;
+                }),
+                600
+            );
 
         $controller = new ApiController();
         $result = $controller->getFiltersWithCounts($pdo, $redisCache);
